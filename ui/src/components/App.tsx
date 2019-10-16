3ae75d682c8040165af556a76bcfa76f98a3985f
Michael <michael,.chi1997@gmail.com>
2019-10-16 09:00:26 -0700

    Created Weight component

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/App.css";
 import NavBar from "./Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Weight from "../components/Weight";
 
 class App extends React.PureComponent {
   render() {
     return (
       <div className="app">
         <NavBar />
+        <Router>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Weight />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
--- ui/src/components/Weight.tsx
+++ ui/src/components/Weight.tsx
@@ -3,9 +3,18 @@ import "../css/Weight.css";
 import moment from "moment";
 import { WeightType, Weight } from "../store//types/WeightTypes";
 import "../css/Weight.css";
+import { Form, Row, Col, ButtonGroup, Button } from "react-bootstrap";
+import "react-day-picker/lib/style.css";
 
-export default class WeightTracker extends React.PureComponent<Weight> {
-  state = {
+type State = {
+  weight: number;
+  month: number;
+  day: number;
+  year: number;
+  weightType: WeightType;
+};
+export default class WeightTracker extends React.PureComponent<{}, State> {
+  state: State = {
     weight: 0,
     month: moment().get("month"),
     year: moment().get("year"),
@@ -17,6 +26,59 @@ export default class WeightTracker extends React.PureComponent<Weight> {
     return (
       <div className="content weight">
         <h1 className="weight-header">Log Weight</h1>
+        <div className="weight-form">
+          <Form>
+            <Form.Group
+              as={Row}
+              className="weight-form-group"
+              controlId="formWeightInput"
+            >
+              <Form.Label className="weight-form-label" column xs="3">
+                Weight
+              </Form.Label>
+              <Col xs="9">
+                <Form.Control type="text" />
+              </Col>
+            </Form.Group>
+            <Form.Group
+              as={Row}
+              className="weight-form-group"
+              controlId="formWeightInput"
+            >
+              <Form.Label className="weight-form-label" column xs="3">
+                Date
+              </Form.Label>
+              <Col xs="9">/></Col>
+            </Form.Group>
+            <Form.Group className="weight-form-group">
+              <ButtonGroup
+                toggle={true}
+                size="sm"
+                className="weight-form-buttongroup"
+              >
+                <Button
+                  active={this.state.weightType === WeightType.LB}
+                  variant="light"
+                  onClick={() => this.setState({ weightType: WeightType.LB })}
+                >
+                  {WeightType.LB}
+                </Button>
+                <Button
+                  active={this.state.weightType === WeightType.KG}
+                  variant="light"
+                  onClick={() => this.setState({ weightType: WeightType.KG })}
+                >
+                  {WeightType.KG}
+                </Button>
+              </ButtonGroup>
+            </Form.Group>
+            <Form.Group className="weight-form-group">
+              <Button className="weight-form-log-button" variant="light">
+                Log
+              </Button>
+            </Form.Group>
+          </Form>
+        </div>
       </div>
     );
   }
